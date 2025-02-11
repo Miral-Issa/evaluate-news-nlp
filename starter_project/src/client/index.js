b381f51c68b666a8e049b79369db976b55155cf1
Miral-Issa <issameral@gmail.com>
2025-02-11 19:21:23 +0200

    babel loader

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,7 +1,8 @@
 // js files
 import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker'
 
-
+console.log(checkForName);
 
 alert("I EXIST")
 // console.log("CHANGE!!");
